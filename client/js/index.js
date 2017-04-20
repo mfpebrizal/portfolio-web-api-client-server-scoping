30b9bf21d90b812660d67fa338239af8a10b0b6a
mfpebrizal <ftm.pebrizal@gmail.com>
2017-04-21 02:12:14 +0700

    finding value in query done

--- client/js/index.js
+++ client/js/index.js
@@ -1,24 +1,47 @@
-// function dataHeading() {
-//     if (document.querySelector("#title-text").innerHTML == "This is Title") {
-//         document.querySelector("#title-text").innerHTML = $("#title-text").attr("data")
-//     } else {
-//         document.querySelector("#title-text").innerHTML = "This is Title"
-//     }
-// }
+function dataHeading() {
 
-$('#home').click(function () {
-  $('#skill-button').css("display","none");
-  $('#meal-button').css("display","none");
-})
+    if ($("#title-text").html() == "This is first Title appeared") {
+      let string = $("#title-text").attr("data")
+      $("#title-text").html(string)
+    }else{
+      $("#title-text").html("This is first Title appeared")
+    }
+}
 
-$('#skills').click(function () {
-  $('#skills').addClass("is-active").siblings().removeClass( "is-active" );
+
+
+function skillButton(name, url) {
+   name = name.replace(/[\[\]]/g, "\\$&");
+   var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+       results = regex.exec(url);
+
+   if (!results) return null;
+   if (!results[2]) return '';
+   return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+let changeHeadingUsingUrl = function() {
+  let link = 'http://127.0.0.1:8080/search?query=ThisIsFromQuery'
+  let data = skillButton('query', link)
+  $('#title-text').text(`${data}`)
+  $( '#title-text' ).append( `<h6 class="subtitle is-6">from ${link}</h6>` );
+}
+
+
+$('#skill').click(function () {
+  $('#skill').addClass("is-active").siblings().removeClass( "is-active" );
+  $('#skill-button').css("display","").siblings().css("display","none");
+  $("#title-text").html("You press Skills in NavBar");
 })
 
 $('#meals').click(function () {
   $('#meals').addClass("is-active").siblings().removeClass( "is-active" );
+  $('#meal-button').css("display","").siblings().css("display","none");
+  $("#title-text").html("You press Meals in NavBar");
 })
 
 $('#home').click(function () {
   $('#home').addClass("is-active").siblings().removeClass( "is-active" );
+  $('#home-button').css("display","").siblings().css("display","none");
+  $("#title-text").html("You press Home in NavBar");
 })
